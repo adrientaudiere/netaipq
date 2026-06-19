b5e8cc6f21774a4760387eff2658d7df74c78a1a
adrientaudiere <adrien.taudiere@zaclys.net>
2026-06-19 09:32:19 +0200

    Improve test

--- tests/testthat/test_netassoc_network_pq.R
+++ tests/testthat/test_netassoc_network_pq.R
@@ -11,7 +11,7 @@ mini_pq <- function() {
 
 test_that("netassoc_network_pq returns a network list with an igraph", {
   skip_if_not_installed("netassoc")
-  res <- netassoc_network_pq(mini_pq(), numnulls = 20)
+  res <- suppressWarnings(netassoc_network_pq(mini_pq(), numnulls = 20))
   expect_type(res, "list")
   expect_true("network_all" %in% names(res))
   expect_s3_class(res$network_all, "igraph")
